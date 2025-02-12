5a617a1e7b82eabcf2944d2b3512ad2f15a56535
trokoch <artrokoch1@gmail.com>
2025-02-13 01:20:10 +0200

    Update 01_newAccount.js

--- 01_newAccount.js
+++ 01_newAccount.js
@@ -11,7 +11,7 @@ const { writeFileSync } = require("fs");
 
 function _generateMnemonic() {
   const strength = 256; // 256 bits, 24 words; default is 128 bits,  12 words
-  const mnemonic = generateMnemonic(wordlist, strength);
+  const mnemonic = generateMnemonic(wordlist, strength); 
   const entropy = mnemonicToEntropy(mnemonic, wordlist);
   return { mnemonic, entropy };
 }
